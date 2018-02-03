36227e20df838afe79b776426251a5ac949878dd
Michael Roy <michael@michaels-macbook-pro.local>
2018-02-03 09:17:31 -0700

    checkpoint: table view configured (not connected with data yet)

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Michael Roy on 2/3/18.
+//  Copyright © 2018 Hello World Corporation. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
